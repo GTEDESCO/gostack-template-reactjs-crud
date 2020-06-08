4e4c18a4ebe3e596a47d4db8c8328a95d9bf6141
Gustavo Tedesco <gustavo_tedesco@hotmail.com>
2020-06-08 11:26:06 -0300

    Update index.tsx

--- src/components/Food/index.tsx
+++ src/components/Food/index.tsx
@@ -28,7 +28,7 @@ const Food: React.FC<IProps> = ({
   const [isAvailable, setIsAvailable] = useState(food.available);
 
   async function toggleAvailable(): Promise<void> {
-    await api.patch(`foods/${food.id}`, {
+    await api.put(`foods/${food.id}`, {
       available: !isAvailable,
     });
     setIsAvailable(!isAvailable);
